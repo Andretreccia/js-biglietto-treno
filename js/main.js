b4ab2905b856bdbd97db7b908e60cd252988f9d0
Andretreccia <andreabtreccia@gmail.com>
2021-10-11 15:41:02 +0200

    Scritti sotto forma di commento gli step che il programma dovrà eseguire

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Chiedere all utente la sua età
+
+//Chiedere all utente quanti km vuole percorrere
+
+//Calcolare il prezzo del biglietto 
+
+//Applicare uno sconto in base all età
+
+//Generare un output per l utente

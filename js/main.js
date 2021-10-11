dfbd14a86179c5bae2aec9cab7609901d31dd2ee
Andretreccia <andreabtreccia@gmail.com>
2021-10-11 16:07:07 +0200

    Richieste all utente le variabili necessarie per il calcolo che dovrà effettuare il programma

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+
+
 //Chiedere all utente la sua età
+const user_age = prompt('Quanti anni hai?')
+
 
 //Chiedere all utente quanti km vuole percorrere
-
+const distance = prompt('Quanti chilometri vuoi percorrere?')
+console.log(distance)
 //Calcolare il prezzo del biglietto 
 
 //Applicare uno sconto in base all età

626d8ab1d16f15e2d28685ba7dbf88d53c4deeee
Andretreccia <andreabtreccia@gmail.com>
2021-10-11 16:25:21 +0200

    Aggiunto calcolo del biglietto con il prezzo al km definito

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 
 
 //Chiedere all utente la sua età
-const user_age = parseInt(prompt('Quanti anni hai?'))
+const user_age = parseInt(prompt('Quanti anni hai?'));
 console.log(user_age)
 
 
 //Chiedere all utente quanti km vuole percorrere
-const distance = parseInt(prompt('Quanti chilometri vuoi percorrere?'))
+const distance = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 console.log(distance)
 //Calcolare il prezzo del biglietto 
-
+const priceForKm = 0.21;
+const priceTicket = (distance * priceForKm)
+console.log(priceTicket)
 //Applicare uno sconto in base all età
 
 //Generare un output per l utente
